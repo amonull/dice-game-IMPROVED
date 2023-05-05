4976f11fff78bda90ce4ee1b0f16ef3888b5d212
amonull <90932550+amonull@users.noreply.github.com>
2023-05-05 19:40:01 +0000

    Removing redundant comments

--- src/utils/rolls.py
+++ src/utils/rolls.py
@@ -31,23 +31,3 @@ def roll_for_one_player() -> int:
             print(f"score is {player_score}")
 
     return player_score
-
-#     @classmethod
-#     def check_winner(self, player1, player2, player_name1, player_name2):
-#         self.player1 = player1
-#         self.player2 = player2
-#         self.player_name1 = player_name1
-#         self.player_name2 = player_name2
-
-#         while player1 == player2:
-#             print("both players have the same score dice will be thrown untill one player is declared the winner.")
-#             draw_roll1 = random.randint(1,6)
-#             draw_roll2 = random.randint(1,6)
-#             self.player1 = (self.player1 + draw_roll1)
-#             self.player2 = (self.player2 + draw_roll2)
-#             print(f"{self.player_name1} has rolled {draw_roll1}")
-#             print(f"{self.player_name2} has rolled {draw_roll2}")
-#         if player1 > player2:
-#             print(f"{self.player_name1} has won! with {self.player1} points")
-#         elif player1 < player2:
-#             print(f"{self.player_name2} has won! with {self.player2} points")
